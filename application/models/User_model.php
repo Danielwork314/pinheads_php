93c24461cac27410c9132bee651cc562ae076b3e
unknown <danielwork314@gmail.com>
2018-11-20 15:33:23 +0800

    201118

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -8,5 +8,29 @@ class User_model extends Base_Model{
         $this->table_name = "user";
     }
     
-    
+    public function login_email($email, $password)
+    {
+        $this->db->select("*");
+        $this->db->from($this->table_name);
+        $this->db->where("email = ", $email);
+        $this->db->join("role", $this->table_name . ".role_id = role.role_id", "left");
+        $this->db->where("password = SHA2(CONCAT(salt,'" . $password . "'),512)");
+
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+    public function login_phone($phone, $password)
+    {
+        $this->db->select("*");
+        $this->db->from($this->table_name);
+        $this->db->where("email = ", $phone);
+        $this->db->join("role", $this->table_name . ".role_id = role.role_id", "left");
+        $this->db->where("password = SHA2(CONCAT(salt,'" . $password . "'),512)");
+
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
 }
